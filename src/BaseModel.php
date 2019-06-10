ca923e21cdfb7ddd918bfae7986faa6175c8f6bc
Luís Victor Quintas <luisvictorquintas@gmail.com>
2019-06-09 23:00:03 -0300

    remove model

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -7,19 +7,4 @@ class BaseModel extends \Illuminate\Database\Eloquent\Model
 {
     use MapRequestFields;
 
-    public $incrementing = false;
-
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-    }
-
-    public static $allowableRelationships = [];
-    public static $allowableFilters = [];
-    public static $defaultIncludes = [];
-    public static $uuidAttributes = [];
-
-    public static function filterByAccess($query, $args = [])
-    {
-    }
 }
